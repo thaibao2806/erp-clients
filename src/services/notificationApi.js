04986de77f6132738a1fd7611105afeba0cac132
thaibao2806 <thaibaosonhai123@gmail.com>
2025-07-30 09:44:23 +0700

    update: update task

--- src/services/notificationApi.js
+++ src/services/notificationApi.js
@@ -2,16 +2,16 @@ import { filterNotification, sendAllUser, url } from "../config/config";
 import axiosInstance from "./axiosInstance";
 
 export const fetchUnreadNotifications = async (userId) => {
-  const res = await axiosInstance.get(url + `/api/notification/unread/${userId}`);
+  const res = await axiosInstance.get( `/api/notification/unread/${userId}`);
   return res.data;
 };
 
 export const markNotificationAsRead = async (id) => {
-  return axiosInstance.put(url + `/api/notification/read/${id}`);
+  return axiosInstance.put( `/api/notification/read/${id}`);
 };
 
 export const getAllNotification = () => {
-  return axiosInstance.get(url + "/api/Notification/user")
+  return axiosInstance.get( "/api/Notification/user")
 }
 
 export const filterNofifications = (userId, keyword, isRead, fromDate, toDate, page, pageSize) => {
